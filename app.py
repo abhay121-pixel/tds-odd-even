ef798fcf605da417bb779673053c674ee8c75174
abhay121-pixel <82321625+abhay121-pixel@users.noreply.github.com>
2022-11-30 20:09:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ placeholder_number = st.empty()
 m = placeholder_number.number_input('Enter your number')
 if float(m)%2 == 0:
 	st.write('even number')
-  else:
-    st.write('odd number')
+else:
+ 	st.write('odd number')
 st.write("Abhaysingh-21f1002151")
